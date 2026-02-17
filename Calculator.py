bb47da1862e3089627b0bc92d105bd7face8a1c2
Rahul Surendran <rahulsurendran154@gmail.com>
2026-02-17 12:38:02 +0530

    Updated Button Variables

--- Calculator.py
+++ Calculator.py
@@ -26,7 +26,7 @@ input_value = StringVar()
 display_text = Entry(main, font=("arial",20,"bold"),textvariable=input_value,bd=30,insertwidth=4,bg="powder blue",justify=RIGHT)
 display_text.grid(columnspan=4)
 
-#Row 1- 7 8 9 +
+#Row 1- 7 8 9 /
 
 btn_7 = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="7",command=lambda: buttonClick(7))
 btn_7.grid(row=1,column=0)
@@ -40,7 +40,7 @@ btn_9.grid(row=1,column=2)
 btn_div = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="/",command=lambda: buttonClick("/"))
 btn_div.grid(row=1,column=3)
 
-#Row 2- 4 5 6 -
+#Row 2- 4 5 6 *
 
 btn_4 = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="4",command=lambda: buttonClick(4))
 btn_4.grid(row=2,column=0)
@@ -54,7 +54,7 @@ btn_6.grid(row=2,column=2)
 btn_mul = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="*",command=lambda: buttonClick("*"))
 btn_mul.grid(row=2,column=3) 
 
-#Row 3- 1 2 3 *
+#Row 3- 1 2 3 -
 
 btn_1 = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="1",command=lambda: buttonClick(1))
 btn_1.grid(row=3,column=0)
@@ -68,7 +68,7 @@ btn_3.grid(row=3,column=2)
 btn_sub = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="-",command=lambda: buttonClick("-"))
 btn_sub.grid(row=3,column=3)
 
-#Row 4- 0 C = /
+#Row 4- C 0 = +
 
 btn_clear = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="C",command=buttonClear)
 btn_clear.grid(row=4,column=0)
